dfa795f5ebb8311565ba5d7ec9f9759e360107c3
patrick <patrick.finnerty@insa-lyon.fr>
2016-11-18 09:38:42 +0100

    Creqtion filechooser pour ouvrir des fichiers xml

---
+++ src/com/hexagone/xml/XMLFileOpener.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.hexagone.xml;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+import com.hexagone.xml.XMLExceptions;
+/**
+ * This class provides the methods to open any xml file on the system.
+ * 
+ *
+ */
+public class XMLFileOpener extends FileFilter {
+
+	
+	private static XMLFileOpener instance = null;
+	private XMLFileOpener(){}
+	protected XMLFileOpener getInstance(){
+		if(instance == null) instance = new XMLFileOpener();
+		return instance;
+	}	
+	
+	public File open () throws XMLException{
+		int returnValue;
+		JFileChooser jFileChooserXML = new JFileChooser();
+		jFileChooserXML.setFileFilter(this);
+		returnValue = jFileChooserXML.showOpenDialog(null);
+		if (returnValue != JFileChooser.APPROVE_OPTION) 
+        	throw new XMLException("Problem opening XML file");
+        return new File(jFileChooserXML.getSelectedFile().getAbsolutePath());
+		
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see javax.swing.filechooser.FileFilter#accept(java.io.File)
+	 */
+	@Override
+	public boolean accept(File f) {
+		if(f == null) return false;
+		if (f.isDirectory()) return true;
+		String extension = getExtension(f);
+		if (extension == null) return false;
+		return extension.contentEquals("xml");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see javax.swing.filechooser.FileFilter#getDescription()
+	 */
+	@Override
+	public String getDescription() {
+		return "XML File";
+	}
+	
+	/**
+	 * This method allows to get file's extension
+	 * @param f the file whose extension we want to extract
+	 * @return file extension as a String, null if the file has no extension
+	 */
+	private String getExtension(File f) {
+	    String filename = f.getName();
+	    int i = filename.lastIndexOf('.');
+	    if (i>0 && i<filename.length()-1) 
+	    	return filename.substring(i+1).toLowerCase();
+	    return null;
+   }
+
+}
---
+++ src/com/hexagone/xml/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package contenant les interactions avec les fichiers XML des plans et des tournées
+ */
+/**
+ * @author patrick
+ *
+ */
+package com.hexagone.xml;
