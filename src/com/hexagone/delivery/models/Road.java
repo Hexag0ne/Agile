141ed8767c83d1c588495764c33cb1361bdfd3ef
patrick <patrick.finnerty@insa-lyon.fr>
2016-11-20 21:44:45 +0100

    Ajouté un peu de JAVAdoc

--- src/com/hexagone/delivery/models/Road.java
+++ src/com/hexagone/delivery/models/Road.java
@@ -4,8 +4,15 @@
 package com.hexagone.delivery.models;
 
 /**
- * @author patrick
- *
+ * This class is part of the map modelling. 
+ * A Road is a link between two Intersections
+ * It carries several information :
+ * <ul>
+ * <li>The two intersections it is linking</li>
+ * <li>it's name</li>
+ * <li>it's length</li>
+ * <li>the average speed motorists travel on this road</li>
+ * </ul> 
  */
 public class Road {
 
