049165a321e7ad746365581fe6717a71560d38c9
Mehdi BAHA <elmehdi.baha@gmail.com>
2016-11-29 11:39:26 +0100

    Add method to main (fake method)

--- src/com/hexagone/delivery/launcher/Main.java
+++ src/com/hexagone/delivery/launcher/Main.java
@@ -1,7 +1,13 @@
 package com.hexagone.delivery.launcher;
 
+import java.util.ArrayList;
+
+import com.hexagone.delivery.algo.DeliveryComputer;
+import com.hexagone.delivery.models.DeliveryQuery;
+import com.hexagone.delivery.models.Map;
+import com.hexagone.delivery.models.Route;
+import com.hexagone.delivery.xml.XMLDeserialiser;
 import com.hexagone.delivery.xml.XMLException;
-import com.hexagone.ui.MainFrame;
 
 /**
  * Classe qui gère le lancement de l'application. Point d'entrée de l'éxécutable
@@ -14,8 +20,31 @@ public class Main {
 	 * @throws XMLException
 	 */
 	public static void main(String[] args) throws XMLException {
-		MainFrame frame = new MainFrame();
+		DeliveryQuery dq = XMLDeserialiser.loadDeliveryQuery();
+		Map map = XMLDeserialiser.loadMap();
+
+		DeliveryComputer dc = new DeliveryComputer(map, dq);
+		Route r = new Route(map, dq, dc);
+		
+		r.generateRoute();
+		r.generateTxt("export/planning.txt");
+		
+		/*MainFrame frame = new MainFrame();
 		frame.setVisible(true);
-		frame.setLocationRelativeTo(null);
+		frame.setLocationRelativeTo(null);*/
+	}
+	
+	public static ArrayList<Integer> getIntersectionsBetween(Integer i1, Integer i2) {
+		ArrayList<Integer> sols = new ArrayList<Integer>();
+		
+		if (i1.equals(6)) {
+			sols.clear(); sols.add(6); sols.add(7); sols.add(12);
+		} else if (i1.equals(12)) {
+			sols.clear(); sols.add(12); sols.add(13); sols.add(8);
+		} else {
+			sols.clear(); sols.add(8); sols.add(7); sols.add(6);
+		}
+		
+		return sols;
 	}
 }
